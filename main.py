4dc56fcfa9a45f5ee3978c9a0f8d91c05d7e2e95
Ashley Rodriguez Vega <150083400+ashxvga@users.noreply.github.com>
2024-03-08 23:55:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,44 @@
 """
-This application will allow the user to generate salaries whether they are an employer or employee. 
+This application will allow the user is to generate salaries wheter they are an employer or employee. 
 -change later
 """
+#My imports
 from tkinter import *
 from PIL import Image, ImageTk
-import openWindows
+import employerWindow
+import employeeWindow
+import helpWindow
+
 
 #-------------Main Window-------
 
-#The first window is the main - where the user identifies as an employer or employee 
+#The first window is the main - where the user identifies as employer or employee 
 mainWindow = Tk()
 mainWindow.title("Salary Calculator: Easy Pay")
-#mainWindow.geometry("500x500") - will change later
+#mainWindow.geometry("915x500") #- will change later
 
 
 
 #-------------Frames for mainWindow------------
-#mainFrame1 (top)- where all the text will go
+#mainFrame1 (top)- where the all the text will go
 #mainFrame2 (middle)- where the images will go
 #mainFrame3 (bottom)- where the buttons will go
 
 
 #My top frame
-mainFrame1 = LabelFrame(mainWindow, padx= 312, pady= 10)
+mainFrame1 = Frame(mainWindow, padx= 50, pady= 10)
 mainFrame1.grid(row = 0, column = 0, rowspan= 10, columnspan= 10)
 
 #My middle frame
-mainFrame2 = LabelFrame(mainWindow, padx= 200, pady= 10)
+mainFrame2 = Frame(mainWindow, padx= 50, pady= 10)
 mainFrame2.grid(row= 10, column= 0, rowspan= 10, columnspan= 10)
 
 #My bottom frame
-mainFrame3 = LabelFrame(mainWindow, padx=360, pady=10)
-mainFrame3.grid(row= 30, column= 0, rowspan= 10, columnspan= 10)
+mainFrame3 = Frame(mainWindow, padx=50, pady=10)
+mainFrame3.grid(row= 20, column= 0, rowspan= 10, columnspan= 10)
 
 #--------Labels----------------
-#Includes the app name and asks the user what they are (employer or employee)
+#Includes the app name and asks the user what are they (employer or employee)
 
 nameLabel = Label(mainFrame1, text= "Salary Calculator", fg = "#FFD300", font =("Comic Sans MS",35))
 nameLabel.grid(row= 0, column= 0)
@@ -50,13 +54,17 @@ whatAreYouLabel.grid(row = 10, column=0)
 
 #-----need to resize them to make it look better/ make it fit
 #Employer image icon
-employerImage = ImageTk.PhotoImage(Image.open("key.png").resize((250,250)))
-employerImageLabel = Label(mainFrame2, image= employerImage)
+employerImage = Image.open("key.png").resize((250,250))
+employerImageTk = ImageTk.PhotoImage(employerImage)
+employerImageLabel = Label(mainFrame2, image= employerImageTk, text="Employer Icon: Holding Key", compound="top")
+employerImageLabel.image = employerImageTk
 employerImageLabel.grid(row = 2, column= 0)
 
 #Employee image icon
-employeeImage = ImageTk.PhotoImage(Image.open("person.png").resize((250,250)))
-employeeImageLabel = Label(mainFrame2, image = employeeImage)
+employeeImage = Image.open("person.png").resize((250,250))
+employeeImageTk = ImageTk.PhotoImage(employeeImage)
+employeeImageLabel = Label(mainFrame2, image = employeeImageTk, text="Employee Icon", compound="top")
+employeeImageLabel.image = employeeImageTk
 employeeImageLabel.grid(row = 2, column= 5)
 
 #----------Buttons----------------
@@ -65,49 +73,36 @@ employeeImageLabel.grid(row = 2, column= 5)
 #Button that takes you to the second window
 #Takes you to the employer window
 
-employerButton = Button(mainFrame3, text = "Employer", command= lambda: openWindows.openEmployerWindow(mainWindow))
+
+employerButton = Button(mainFrame3, text = "Employer", command= lambda: openEmployer())
 employerButton.grid(row = 0, column= 0)
 
 #Button that takes you to the third window
 #Takes you to the employee window
 
-employeeButton = Button(mainFrame3, text = "Employee", command= lambda: openWindows.openEmployeeWindow(mainWindow))
-employeeButton.grid(row = 0, column= 5)
+employeeButton = Button(mainFrame3, text = "Employee", command= lambda: openEmployee())
+employeeButton.grid(row = 0, column= 3)
 
+#Button that opens the fourth window
+#Takes you to the help window
+helpButton = Button(mainFrame3, text = "Help",command=helpWindow.open_help_window)
+helpButton.grid(row = 0, column= 5)
 
-"""
-#doesn't work
-#---------Frames for second window
-#trial #1
-secondWindow = openWindows.openEmployerWindow(mainWindow)
-secondFrame1 = LabelFrame(secondWindow, padx= 312, pady= 10)
-secondFrame1.grid(row = 0, column = 0, rowspan= 10, columnspan= 10)
-SecondNameLabel = Label(secondFrame1, text= "Your Name", fg = "#FFD300", font =("Arial",12))
-SecondNameLabel.grid(row= 0, column= 0)
+#This will make sure to only allow the user to have one type of window open
+#Meaning if the employer window is open, the employee window closes and viceversa
 
-#trial #2
-#My top frame
-secondFrame1 = LabelFrame(secondWindow, padx= 312, pady= 10)
-secondFrame1.grid(row = 0, column = 0, rowspan= 10, columnspan= 10)
+#This closes the employee window (if it's open) and opens the employer window
+def openEmployer():
+    if employeeWindow.employee_window is not None and employeeWindow.employee_window.winfo_exists():
+        employeeWindow.employee_window.destroy() 
+    employerWindow.openEmployerWindow(mainWindow)  
 
-#doesn't work either
+#This closes the employer window (if it's open) and opens the employee window
+def openEmployee():
+    if employerWindow.employer_window is not None and employerWindow.employer_window.winfo_exists():
+        employerWindow.employer_window.destroy()
+    employeeWindow.openEmployeeWindow(mainWindow) 
 
-#My middle frame
-secondFrame2 = LabelFrame(openWindows.openEmployerWindow(mainWindow), padx= 200, pady= 10)
-secondFrame2.grid(row= 10, column= 0, rowspan= 10, columnspan= 10)
-
-#My bottom frame
-secondFrame3 = LabelFrame(openWindows.openEmployerWindow(secondWindow), padx=360, pady=10)
-secondFrame3.grid(row= 30, column= 0, rowspan= 10, columnspan= 10)
-
-SecondNameLabel = Label(secondFrame1, text= "Your Name", fg = "#FFD300", font =("Arial",12))
-SecondNameLabel.grid(row= 0, column= 0)
-"""
-
-mainWindow.mainloop()
-
-employeeButton = Button(mainFrame3, text = "Employee")
-employeeButton.grid(row = 0, column= 5)
 
 
 mainWindow.mainloop()
